3eb1f769432a53f9d90f2a7a3af2a26c2107c509
Anand Nanwana <121349107+anand-nanwana14@users.noreply.github.com>
2023-12-10 11:53:13 +0530

    items.js

---
+++ items.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const Item = require('../models/item');
+
+// Get all items
+router.get('/', async (req, res) => {    res.status(201).json(newItem);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Get a specific item
+router.get('/:id', getItem, (req, res) => {
+  res.json(res.item);
+});
+
+// Update an item
+router.patch('/:id', getItem, async (req, res) => {
+  if (req.body.name != null) {
+    res.item.name = req.body.name;
+  }
+  if (req.body.description != null) {
+    res.item.description = req.body.description;
+  }
+  try {
+    const updatedItem = await res.item.save();
+    res.json(updatedItem);
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Create an item
+router.post('/', async (req, res) => {
+  const item = new Item({
+    name: req.body.name,
+    description: req.body.description
+  });
+
+  try {
+    const newItem = await item.save();
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete an item
+router.delete('/:id', getItem, async (req, res) => {
+  try {
+    await res.item.remove();
+    res.json({ message: 'Item deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getItem(req, res, next) {
+  let item;
+  try {
+    item = await Item.findById(req.params.id);
+    if (item == null) {  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.item = item;
+  next();
+}
+
+module.exports = router;
+      return res.status(404).json({ message: 'Item not found' });
+    }
+
+
+
+    
